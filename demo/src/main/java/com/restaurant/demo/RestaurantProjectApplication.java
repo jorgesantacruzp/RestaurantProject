5e44357b7576135beab67224d26074e45f02c66f
juanstcp26 <juanstcp26@gmail.com>
2022-02-18 21:51:50 -0500

    Spring Boot Project

---
+++ demo/src/main/java/com/restaurant/demo/RestaurantProjectApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantProjectApplication.class, args);
+	}
+
+}
