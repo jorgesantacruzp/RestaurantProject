2f3e5ea75c7f977cf3efa0058fced5e1f11b10c7
juanstcp26 <juanstcp26@gmail.com>
2022-03-14 12:31:22 -0500

    login

--- demo/src/main/java/com/restaurant/demo/repository/UserRepository.java
+++ demo/src/main/java/com/restaurant/demo/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.restaurant.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.restaurant.demo.entity.User;
 
+@Transactional(readOnly=true)
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	Optional<User> findByEmail(String email);
 }
